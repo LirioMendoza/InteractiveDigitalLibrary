5653b58c21f0aa685a7c7dc5eff62a5a2ccec056
“MEJIA943” <lalo.ejmv@gmail.com>
2023-10-26 00:47:01 -0600

    FEAT(Downloads_module): Descripción Inicial frontend downloads

---
+++ downloads_module/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+function DownloadPage(){
+  return(
+    <div>
+      <h1>Download Module</h1>
+      <Button variant="contained"> Descarga </Button>
+      <Button variant="contained"> Descarga </Button>
+    </div>
+  )
+}
+
+export default DownloadPage
