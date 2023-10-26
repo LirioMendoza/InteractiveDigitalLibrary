f9a66c8f9fc07b385f128ebc70ea9f1fbad3a181
ClaraLuzCA <clara.cornejo@ingenieria.unam.edu>
2023-10-25 23:18:22 -0600

    FEAT (Resource_catalog_module): add app Resource catalog module

---
+++ resource_catalog_module/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import  BasicAppBar from '@/components/basic-app-bar';
+
+import BasicAppGrid from '@/components/basic-app-bar';
+import RootLayout from './layout';
+
+export default function Home() {
+  return (
+
+    
+    <BasicAppGrid />
+
+  )
+}
