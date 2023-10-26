d30edd815a48d4b18442dfd9e7bd0c08c025975f
ErnestoChav17 <98865119+ChavezVazquezErnesto@users.noreply.github.com>
2023-10-26 00:41:43 -0600

    FEAT (navegation_routing_app: create app

---
+++ Navigation_routing/app/page.js
@@ -0,0 +1,25 @@
+"use client";
+import Navbar from "@/components/navbar/Navbar";
+import { Container } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Login from "./login/page";
+import Catalog from "./catalog/page";
+import Home from "./home/page";
+
+export default function App(){
+  return (
+    <>
+    <Navbar />
+    <Container sx={{ mt: 5}}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
+      </Router>
+    </Container>
+    </>
+  );
+}
