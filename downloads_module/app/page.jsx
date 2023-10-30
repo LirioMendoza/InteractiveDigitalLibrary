7d238bbd75dea99e6832a2b3f29a4f18c8535c4a
“MEJIA943” <lalo.ejmv@gmail.com>
2023-10-30 14:50:25 -0600

    FEAT(Downloads_module):First download of the first book

--- downloads_module/app/page.jsx
+++ downloads_module/app/page.jsx
@@ -1,13 +1,37 @@
-import { Button } from "@mui/material";
+'use client';
+import { Button } from '@mui/material';
+import DownloadIcon from '@mui/icons-material/Download';
 
-function DownloadPage(){
-  return(
+const DownloadButton = (fileURL) => {
+  const handleDownload = (fileURL1) => {
+    const link = document.createElement('a');
+    link.href = fileURL1;
+    link.setAttribute('download', 'RAYUELA.pdf');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <Button
+      onClick={() => handleDownload(fileURL) }
+      variant="contained"
+      color="primary"
+      startIcon={<DownloadIcon />}
+    >
+      Descargar
+    </Button>
+  );
+
+};
+const Page = () => {
+  const fileURL = 'https://drive.google.com/uc?id=1ezbfcYcVFiXMzRpJG5-tlDl6Y9pkjRk1&export=download';
+  return (
     <div>
-      <h1>Download Module</h1>
-      <Button variant="contained"> Descarga </Button>
-      <Button variant="contained"> Descarga </Button>
+      <h1>DOWNLOAD</h1>
+      {DownloadButton(fileURL)}
     </div>
-  )
-}
 
-export default DownloadPage
+  );
+};
+export default Page;
