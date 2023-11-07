db8368f0855939e49e3cf88ae9e9a0113cb9e7c3
“MEJIA943” <lalo.ejmv@gmail.com>
2023-11-07 01:23:12 -0600

    FIX(Downloads_module):Integrating testing and log in main

--- Navigation_routing/tests/index.test.js
+++ Navigation_routing/tests/index.test.js
@@ -2,6 +2,27 @@ import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import CommentForm from '@/components/comments/commentForm';
+import DownloadButton from '@/components/navbar/downloads';
+
+describe('DownloadButton', () => {
+  it('renders the button with text Descargar', () => {
+    const fileURL = 'https://drive.google.com/uc?id=1ezbfcYcVFiXMzRpJG5-tlDl6Y9pkjRk1&export=download';
+    render(<DownloadButton fileURL={fileURL} />);
+    const buttonElement = screen.getByRole('button', { name: /Descargar/i });
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  it('simulates the download when the button is clicked', () => {
+    const fileURL = 'https://drive.google.com/uc?id=1ezbfcYcVFiXMzRpJG5-tlDl6Y9pkjRk1&export=download';
+    const handleDownloadSpy = jest.spyOn(DownloadButton, 'handleDownload');
+    render(<DownloadButton fileURL={fileURL} />);
+    const buttonElement = screen.getByRole('button', { name: /Descargar/i });
+
+    fireEvent.click(buttonElement);
+
+    expect(handleDownloadSpy).toHaveBeenCalled();
+  });
+});
 
 test('renders CommentForm component', () => {
     render(<CommentForm addComment={() => {}} />);
