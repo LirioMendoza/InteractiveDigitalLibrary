9e0f80f718c4b42d04c8d3cbf103f7f1baa1f8c6
“MOONeeS0408” <moonee0408@gmail.com>
2023-12-04 21:46:45 -0600

    FIX(fix-developer-v2.0): small changes in html elements and code documentation

--- digital-library/components/comments/Rating-Stars.jsx
+++ digital-library/components/comments/Rating-Stars.jsx
@@ -1,6 +1,10 @@
 import StarIcon from '@mui/icons-material/Star';
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
-import { createTheme, ThemeProvider } from '@mui/material/';
+import { ThemeProvider } from '@mui/material/';
+
+/* 
+Description: Handles rating Stars between Filled and Empty
+*/ 
 
 const RatingStars = ({ rating }) => {
   console.info({ level: "INFO", message: "Trying to show RaitingStarts from comments."});
@@ -12,36 +16,19 @@ const RatingStars = ({ rating }) => {
       <StarOutlineIcon color="secondary.main" key={index} />
     ));
 
-    const theme = createTheme({
-      palette: {
-        mode: 'light',
-        primary: {
-          main: '#29a493',
-          dark: '#1C7266',
-          contrastText: '#fff',
-        },
-        secondary: {
-          main: '#7c2ec1',
-          light: '#9657CD',
-          dark: '#562087',
-          contrastText: '#fff',
-        },
-      },
-    });
-
     console.info({ level: "DEBUG", message: "Rating value.", data: rating});
     console.log({level: "SUCCESS", message: "Star raiting is displayed."});
 
     return (
       <ThemeProvider theme={theme}>
-        <div style={{ display: 'flex' }}>
+        <Box type='div' style={{ display: 'flex' }}>
           {filledStars}
           {emptyStars}
-        </div>
+        </Box>
       </ThemeProvider>
     );
   } catch (error) {
-    console.error({ level: "ERROR", message: "An error occurred with RaitingStarts.", error});
+      console.error({ level: "ERROR", message: "An error occurred with RaitingStarts.", error});
     return null; 
   }
 };
